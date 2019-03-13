674911445894a3286668e7c50db18752d67c7318
Christopher Ravosa <Christopher.ravosa1@marist.edu>
2019-03-13 12:03:36 -0400

    Menu added, quickSort almost done

--- Algorithms2Ravosa/OrganizerRavosa.java
+++ Algorithms2Ravosa/OrganizerRavosa.java
@@ -20,6 +20,13 @@ public class OrganizerRavosa {
 		String line = null;
 		String[] itemsArray = new String[666];
 		int counter = 0;
+		int mergeComparisons = 0;
+		int i = 0;
+		String[] selectionArray = new String[666];
+		String[] insertionArray = new String[666];
+		String[] mergeArray = new String[666];
+		String[] quickArray = new String[666];
+		boolean menu = true;
 		
 //---------------------END OF VARIABLES---------------------------------------
 				
@@ -47,11 +54,50 @@ public class OrganizerRavosa {
 				
 //----------------------END OF READING IN FILE--------------------------------				
 		
-		mergeSort(itemsArray);
+		for(i = 0; i < itemsArray.length; i++)
+			selectionArray[i] = itemsArray[i];
 		
-		for(int i = 0; i < itemsArray.length; i++)
-			System.out.println((i+1) + ". " + itemsArray[i]);
+		for(i = 0; i < itemsArray.length; i++)
+			insertionArray[i] = itemsArray[i];
 		
+		for(i = 0; i < itemsArray.length; i++)
+			mergeArray[i] = itemsArray[i];
+		
+		for(i = 0; i < itemsArray.length; i++)
+			quickArray[i] = itemsArray[i];
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("-----COMPARISONS IN EACH SORT-----");
+		selectionSort(selectionArray);
+		insertionSort(insertionArray);
+		mergeSort(mergeArray);
+		quickSort(quickArray);
+		System.out.println("----------------------------------");
+			
+		/*
+		while (menu == true) {
+			System.out.println("Would you like to print the sorted array?");
+			System.out.println("Type 'YES' to print, 'NO' to exit");
+		
+			String choice = input.nextLine();
+			switch(choice) {
+			case "YES":
+				for(i = 0; i < selectionArray.length; i++)
+					System.out.println((i + 1) + ". " + selectionArray[i]);
+				break;
+			case "NO":
+				System.out.println("Goodbye!");
+				menu = false;
+				break;
+			default:
+				System.out.println("Invalid input!");
+				break;
+			}//case
+		}//while
+		
+		input.close();
+		//*/
 	}//main
 	
 	
@@ -76,14 +122,8 @@ public class OrganizerRavosa {
 			myArray[i] = temp;
 		}//outer for
 		
-		for (i = 0; i < 666; i++)
-			System.out.println((i+1) + ". " + myArray[i]);
-		
-		System.out.println();
-		System.out.println("Comparisons in selection sort: " + comparisons);
-		
+		System.out.println("Selection Sort: " + comparisons);
 	}//selectionSort
-	
 	
 	public static void insertionSort(String[] myArray) {
 		int i = 1;
@@ -103,32 +143,27 @@ public class OrganizerRavosa {
 			myArray[j + 1] = key;
 		}//for
 		
-		for (i = 0; i < 666; i++)
-			System.out.println((i+1) + ". " + myArray[i]);
-		
-		System.out.println();
-		System.out.println("Comparisons in insertion sort: " + comparisons);
-		
+		System.out.println("Insertion Sort: " + comparisons);
 	}//insertionSort
-	
-	
+		
 	public static String[] mergeSort(String[] myArray) {
 		int length = myArray.length;
 		int comparisons = 0;
 		
-		if (length <= 1) { 
-			return myArray;
-		}//if
-		String[] listLeft = new String[length/2];
-		String[] listRight = new String[length - listLeft.length];
-        System.arraycopy(myArray, 0, listLeft, 0, listLeft.length);
-        System.arraycopy(myArray, listLeft.length, listRight, 0, listRight.length);
+		if (length > 1) {
+			String[] listLeft = new String[length/2];
+			String[] listRight = new String[length - listLeft.length];
+        	System.arraycopy(myArray, 0, listLeft, 0, listLeft.length);
+        	System.arraycopy(myArray, listLeft.length, listRight, 0, listRight.length);
         
-		mergeSort(listLeft);
-		mergeSort(listRight);
+			mergeSort(listLeft);
+			mergeSort(listRight);
+			
+			comparisons = comparisons + merge(myArray, listLeft, listRight);
+		}//else
 		
-		merge(myArray, listLeft, listRight);
-		
+		if (length == 666)
+			System.out.println("Merge Sort: " + comparisons);
 		return myArray;
 	}//mergeSort
 	
@@ -142,22 +177,57 @@ public class OrganizerRavosa {
 			if(listLeft[indexLeft].compareTo(listRight[indexRight]) < 0) {
 				myArray[indexOfMerged] = listLeft[indexLeft];
 				indexLeft++;
-				comparisons++;
 			}//if
 			else {
 				myArray[indexOfMerged] = listRight[indexRight];
 				indexRight++;
 			}//else
 			indexOfMerged++;
+			comparisons++;
 		}//while
+		
 		System.arraycopy(listLeft, indexLeft, myArray, indexOfMerged, listLeft.length - indexLeft);
         System.arraycopy(listRight, indexRight, myArray, indexOfMerged, listRight.length - indexRight);
         
         return comparisons;
 	}//merge
 	
-	public static void quickSort() {
+	public static String[] quickSort(String[] myArray) {
+		int n = myArray.length;
+		String pivot = "";
+		Random r = new Random();
+		int low = 0;
+		int high = myArray.length;
+		int result = r.nextInt(high-low) + low;
+		int i = 0;
+		int counterLeft = -1;
+		int counterRight = -1;
 		
+		if (n<=1) {
+			return myArray;
+		}//if
+		
+		pivot = myArray[result];
+		
+		String[] left = new String[n/2];
+		String[] right = new String[n - left.length];
+		
+		for(i = 0; i < myArray.length; i++) {
+			if (myArray[i].compareTo(pivot) < 0) {
+				counterLeft++;
+				left[counterLeft] = myArray[i];
+			}//if
+			else if(myArray[i].compareTo(pivot) > 0) {
+				counterRight++;
+				right[counterRight] = myArray[i];
+			}//else if
+		}//for
+		
+		quickSort(left);
+		quickSort(right);
+		
+		System.out.println("Aye lmao");
+		return myArray;
 	}//quickSort
 	
 }//OrganizerRavosa
