820739c55b1789511730fc4d53b8be18279091e9
Christopher Ravosa <Christopher.ravosa1@marist.edu>
2019-03-14 01:41:23 -0400

    Working on hashing and binary search

--- Algorithms2Ravosa/OrganizerRavosa.java
+++ Algorithms2Ravosa/OrganizerRavosa.java
@@ -13,6 +13,10 @@ import java.util.*;
 
 public class OrganizerRavosa {
 
+	static final int HASH_TABLE_SIZE = 250;
+	static final int LINES_IN_FILE = 666;
+	static int[] hashValues = new int[666];
+	
 	public static void main(String[] args) {
 		//Variables for reading file
 		String fileName = "magicItems.txt";
@@ -83,11 +87,18 @@ public class OrganizerRavosa {
 			randomsArray[i] = quickArray[randomValue];
 		}//for
 		
-		//linearSearch(quickArray, randomsArray);
-		
-		//for (i = 0; i < randomsArray.length; i++)
-		//	System.out.println("Found: " + binarySearch(selectionArray, 0, 665, randomsArray[i]));
-	
+//--------------------------END OF SEARCHES------------------------------------	
+
+		/*
+		int hashCode = 0;
+		for (i = 0; i < LINES_IN_FILE; i++) {
+			System.out.print(i + 1);
+			System.out.print(". " + itemsArray[i] + " - ");
+			hashCode = makeHashCode(itemsArray[i]);
+			System.out.format("%03d%n", hashCode);
+			hashValues[i] = hashCode;
+		}//for
+		*/
 	}//main
 	
 	public static void selectionSort(String[] myArray) {
@@ -197,36 +208,39 @@ public class OrganizerRavosa {
 	}//merge
 	
 	public static int quickSort(String[] myArray, int lowIndex, int highIndex) {
-		String pivot = myArray[lowIndex + (highIndex - lowIndex)/2];
-		String temp = "";
-		int i = lowIndex;
-		int j = highIndex;
+		int partitionValue = 0;
 		int comparisons = 0;
 		
-		while (i <= j) {
-			while (myArray[i].compareTo(pivot) < 0) {
-				i++;
-				comparisons++;
-			}//while
-			while (myArray[j].compareTo(pivot) > 0) {
-				j--;
-				comparisons++;
-			}//while
-			if (i <= j) {
-				temp = myArray[i];
-				myArray[i] = myArray[j];
-				myArray[j] = temp;
-				i++;
-				j--;
-			}//if
-		}//while
-		if(lowIndex < j)
-			comparisons = comparisons + quickSort(myArray, lowIndex, j);
-		if(i < highIndex)
-			comparisons = comparisons + quickSort(myArray, i, highIndex);
+		if (lowIndex < highIndex) {
+			comparisons++;
+			//partition here
+			String pivot = myArray[highIndex];
+			int i = (lowIndex - 1);
+			for (int j = lowIndex; j < highIndex; j++) {
+				if(myArray[j].compareTo(pivot) <= 0) {
+					i++;
+					comparisons++;
+					
+					String temp = myArray[i];
+					myArray[i] = myArray[j];
+					myArray[j] = temp;
+				}//if
+			}//for
+			
+			String temp = myArray[i + 1];
+			myArray[i + 1] = myArray[highIndex];
+			myArray[highIndex] = temp;
+			
+			partitionValue = i + 1;
+			
+			quickSort(myArray, lowIndex, partitionValue-1);
+			quickSort(myArray, partitionValue+1, highIndex);
+		}//if
 		
 		return comparisons;
-	}//while
+	}//quickSort
+	
+//--------------------------END OF SORT METHODS--------------------------------
 	
 	public static void linearSearch(String[] searchArray, String[] randomValues) {
 		int j =0;
@@ -267,4 +281,96 @@ public class OrganizerRavosa {
 		
 	}//binarySearch
 	*/
+
+//-------------------------END OF SEARCH METHODS-------------------------------
+	
+	static int makeHashCode(String str) {
+		str = str.toUpperCase();
+		int length = str.length();
+		int letterTotal = 0;
+		final int HASH_TABLE_SIZE = 250;
+		
+		// Iterate over all letters in the string, totaling their ASCII values.
+		for (int i = 0; i < length; i++) {
+			char thisLetter = str.charAt(i);
+			int thisValue = (int)thisLetter;
+			letterTotal = letterTotal + thisValue;
+		   
+			//Test: print the char and the hash.
+			/* 
+			System.out.print(" ["); 
+			System.out.print(thisLetter); 
+			System.out.print(thisValue); 
+			System.out.print("] "); 
+			// */
+		}//for
+		
+		// Scale letterTotal to fit in HASH_TABLE_SIZE.
+		int hashCode = (letterTotal * 1) % HASH_TABLE_SIZE;  // % is the "mod" operator
+		   
+		return hashCode;
+	}//makeHashCode
+
+	static void analyzeHashValues(int[] hashValues) {
+		System.out.println("\nHash Table Usage:");
+		
+		// Sort the hash values.
+		Arrays.sort(hashValues);  // This is a "dual-pivot" quicksort.
+		
+		// Test: print the sorted hash values.
+		/* 
+		for (int i=0; i < LINES_IN_FILE; i++) { 
+			System.out.println(hashValues[i]); 
+		} 
+		// */
+		
+		// Create a histogram-like report based on the count of each unique hash value,
+		// count the individual entry size,
+		// the total space used (in items),
+		// and the standard deviation of their distribution over the hash table.
+		int asteriskCount = 0;
+		int[] bucketCount = new int[HASH_TABLE_SIZE];
+		int totalCount = 0;
+		int arrayIndex = 0;
+		
+		for (int i=0; i < HASH_TABLE_SIZE; i++) {
+			System.out.format("%03d ", i);
+			asteriskCount = 0;
+			while ( (arrayIndex < LINES_IN_FILE) && (hashValues[arrayIndex] == i) ) {
+				System.out.print("*");
+				asteriskCount = asteriskCount + 1;
+				arrayIndex = arrayIndex + 1;
+			}
+			System.out.print(" ");
+			System.out.println(asteriskCount);
+			bucketCount[i] = asteriskCount;
+			totalCount = totalCount + asteriskCount;
+		}
+		   
+		System.out.print("Average load (count): ");
+		float averageLoad = (float) totalCount / HASH_TABLE_SIZE;
+		System.out.format("%.2f%n", averageLoad);
+		   
+		System.out.print("Average load (calc) : ");
+		averageLoad = (float) LINES_IN_FILE / HASH_TABLE_SIZE;
+		System.out.format("%.2f%n", averageLoad);
+		   
+		System.out.print("Standard Deviation: ");
+		// TODO: Refactor this into its own method.
+		         double sum = 0;
+		         for (int i=0; i < HASH_TABLE_SIZE; i++) {
+		            // For each value in the array...
+		            // ... subtract the mean from each one ...
+		            double result = bucketCount[i] - averageLoad;
+		            // ... and square the result.
+		            double square = result * result;
+		            // Sum all of those squares.
+		            sum = sum + square;
+		         }
+		         // Divide the sum by the number of values ...
+		         double temp = sum / HASH_TABLE_SIZE;
+		         // ... and take the square root of that.
+		         double stdDev = Math.sqrt(temp);
+		         System.out.format("%.2f%n", stdDev);
+		}
 }//OrganizerRavosa
